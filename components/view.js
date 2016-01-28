6fd6fb5d00c8b6cf96e5afaae0cffa6d86723d15
freeman <the.freeman.lab@gmail.com>
2016-01-27 22:23:17 -0500

    change where camera movement happens

--- components/view.js
+++ components/view.js
@@ -11,11 +11,21 @@ function View (type, gl) {
     var camera = lookat()
     camera.up = [0, 0, 1]
     camera.target = [0, 0, 0]
-    camera.position = [15, 15, 200]
+    camera.position = [1, 0, 75]
     this.camera = camera
   }
 
   if (type === 'orbit') {
     this.camera = orbit(gl.canvas)
   }
+}
+
+View.prototype.update = function () {
+  if (this.type === 'orbit') this.camera.tick()
+}
+
+View.prototype.move = function (transform) {
+  var t = transform.translation
+  this.camera.target = [t[0], t[1] + 20, 1]
+  this.camera.position = [t[0], t[1] - 60, 30]
 }
